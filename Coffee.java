f5267c2d87c7dd8ece0423f6ef84d102a996e96c
tbrooks5 <tbrooks5@ggc.edu>
2014-09-19 16:19:47 -0400

    Create Coffee.java
    created coffee class for storing coffee info

---
+++ Coffee.java
@@ -0,0 +1,14 @@
+public class Coffee
+{
+  private boolean hasSugar;
+  private boolean hasCreamer;
+  private boolean isHot;
+  private double price;
+  
+  public Coffee(double price)
+  {
+    this.price = price;
+  }
+  
+  // TODO: Please implement tomorrow
+}
